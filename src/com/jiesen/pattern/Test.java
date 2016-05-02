9f8e259e99c6b5000e0c4e77d57307f1af6a3203
sen <jiesenboor@gmail.com>
2016-05-02 22:03:34 +0800

    Initial commit

---
+++ src/com/jiesen/pattern/Test.java
@@ -0,0 +1,10 @@
+package com.jiesen.pattern;
+
+/**
+ * Created by sen on 16-5-2.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
