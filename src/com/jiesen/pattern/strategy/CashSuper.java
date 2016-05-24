eff50fa2e8d1556adaf6ac29cd0e10e89f9f5e76
sen <jiesengoor@gmail.com>
2016-05-24 14:43:28 +0800

    add file

---
+++ src/com/jiesen/pattern/strategy/CashSuper.java
@@ -0,0 +1,7 @@
+package com.jiesen.pattern.strategy;
+
+/**
+ * Created by sen on 16-5-24.
+ */
+public class CashSuper {
+}
---
+++ src/com/jiesen/pattern/strategy/Demo.java
@@ -0,0 +1,9 @@
+package com.jiesen.pattern.strategy;
+
+/**
+ *
+ * Created by sen on 16-5-23.
+ */
+public class Demo {
+
+}
